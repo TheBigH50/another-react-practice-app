3106981a87379d01c6ad6ffb86c6919ece10924d
TheBigH50 <HFangmeier@outlook.com>
2022-09-06 10:09:09 -0500

    following along

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+class Heading extends Component{
+  render() {
+    return <h1>{this.props.title}</h1>;
+  }
+}
+
+class App extends Component {
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Heading title="Hello World!" />
+       <Heading title="This is react" />
+       <Heading title="Learing about Props" />
+      </header>
+    </div>
+  );
+}
+}
+
+export default App;
